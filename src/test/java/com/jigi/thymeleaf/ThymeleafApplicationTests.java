83d1827a843e3d59d89bca020b9ae02db4929996
gapjung <jigi@jigi.net>
2023-01-10 21:24:29 +0900

    initial commit

---
+++ src/test/java/com/jigi/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jigi.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
